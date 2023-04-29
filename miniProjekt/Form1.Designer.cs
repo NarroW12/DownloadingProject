58ba4fb599c66d3c44c96e5cfe5f78c89e089a72
Narrow12 <jakub.omie@gmail.com>
2023-04-29 04:07:04 +0200

    Dodałem przyciski process bar itd do wyglądu oraz napisałem funcje które ni chuja nie działają

--- miniProjekt/Form1.Designer.cs
+++ miniProjekt/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            Startbutton = new Button();
+            progressBar = new ProgressBar();
+            resultTextBox = new TextBox();
+            statusLabel = new Label();
+            cancelButton = new Button();
+            SuspendLayout();
+            // 
+            // Startbutton
+            // 
+            Startbutton.Location = new Point(30, 254);
+            Startbutton.Name = "Startbutton";
+            Startbutton.Size = new Size(94, 29);
+            Startbutton.TabIndex = 0;
+            Startbutton.Text = "Start";
+            Startbutton.UseVisualStyleBackColor = true;
+            Startbutton.Click += Startbutton_ClickAsync;
+            // 
+            // progressBar
+            // 
+            progressBar.Location = new Point(30, 199);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(125, 29);
+            progressBar.TabIndex = 1;
+            // 
+            // resultTextBox
+            // 
+            resultTextBox.Location = new Point(224, 243);
+            resultTextBox.Name = "resultTextBox";
+            resultTextBox.Size = new Size(125, 27);
+            resultTextBox.TabIndex = 2;
+            // 
+            // statusLabel
+            // 
+            statusLabel.AutoSize = true;
+            statusLabel.Location = new Point(209, 289);
+            statusLabel.Name = "statusLabel";
+            statusLabel.Size = new Size(50, 20);
+            statusLabel.TabIndex = 3;
+            statusLabel.Text = "label1";
+            // 
+            // cancelButton
+            // 
+            cancelButton.Location = new Point(61, 314);
+            cancelButton.Name = "cancelButton";
+            cancelButton.Size = new Size(94, 29);
+            cancelButton.TabIndex = 4;
+            cancelButton.Text = "Cancel";
+            cancelButton.UseVisualStyleBackColor = true;
+            cancelButton.Click += cancelButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(cancelButton);
+            Controls.Add(statusLabel);
+            Controls.Add(resultTextBox);
+            Controls.Add(progressBar);
+            Controls.Add(Startbutton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button Startbutton;
+        private ProgressBar progressBar;
+        private TextBox resultTextBox;
+        private Label statusLabel;
+        private Button cancelButton;
     }
 }
--- miniProjekt/Form1.cs
+++ miniProjekt/Form1.cs
@@ -1,10 +1,114 @@
+using System.Net;
+
 namespace miniProjekt
 {
     public partial class Form1 : Form
     {
+        private CancellationTokenSource cancellationTokenSource;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private int DownloadAndProcessData(int taskIndex, int numberOfItems, IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            int result = 0;
+
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+
+                // Simulate downloading and processing data
+                string data = DownloadData($"http://example.com/task{taskIndex}/item{i}");
+                int processedData = ProcessData(data);
+
+                result += processedData;
+
+                // Report progress
+                int completedItems = (taskIndex * numberOfItems) + i + 1;
+                progress.Report(completedItems);
+            }
+
+            return result;
+        }
+        private string DownloadData(string url)
+        {
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    return webClient.DownloadString(url);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Wyst¹pi³ wyj¹tek: " + ex.Message);
+                    return null;
+                }
+            }
+        }
+
+        private int ProcessData(string data)
+        {
+            // Simulate processing data
+            Thread.Sleep(100);
+
+            return data.Length;
+        }
+
+        private async void Startbutton_ClickAsync(object sender, EventArgs e)
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            progressBar.Value = 0;
+            resultTextBox.Clear();
+
+            try
+            {
+                int numberOfTasks = 10;
+                int numberOfItemsPerTask = 100;
+                int totalNumberOfItems = numberOfTasks * numberOfItemsPerTask;
+
+                IProgress<int> progress = new Progress<int>(value =>
+                {
+                    progressBar.Value = value;
+                    statusLabel.Text = $"Completed {value} out of {totalNumberOfItems}";
+                });
+
+                // Create tasks
+                Task<int>[] tasks = new Task<int>[numberOfTasks];
+                for (int i = 0; i < numberOfTasks; i++)
+                {
+                    tasks[i] = Task.Factory.StartNew(() => DownloadAndProcessData(i, numberOfItemsPerTask, progress, cancellationTokenSource.Token),
+                        cancellationTokenSource.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+                }
+
+                // Wait for all tasks to complete and sum up the results
+                int totalResult = 0;
+                for (int i = 0; i < numberOfTasks; i++)
+                {
+                    int result = await tasks[i];
+                    totalResult += result;
+                }
+
+                resultTextBox.Text = $"Total result: {totalResult}";
+                statusLabel.Text = "Done";
+            }
+            catch (OperationCanceledException)
+            {
+                statusLabel.Text = "Cancelled";
+            }
+            catch (Exception ex)
+            {
+                statusLabel.Text = $"Error: {ex.Message}";
+            }
+
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource?.Cancel();
         }
     }
 }
